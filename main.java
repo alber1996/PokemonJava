5d264b93b983e075070f438e0c9d2d9b51490ce6
ishmilan <ish.professional@gmail.com>
2016-02-11 01:49:54 +0100

    main v0.3

--- main.java
+++ main.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 /*
  * Clase Principal:(Menú de modos de juego)
  * Por Ish Milan
- * V0.2
+ * V0.3
  */
 public class main {
 public static Scanner sc= new Scanner(System.in);
 public static void main(String[] args) throws IOException {
 	String opc = "1";
 	do {
-	System.out.println(
+		System.out.print("\033[H\033[2J");
+		System.out.println(
 "                                     .::.\n"+
 "                                   .;:**'\n"+
 "                                   `\n"+
@@ -24,15 +25,21 @@ public static void main(String[] args) throws IOException {
 "      ?MMM>   YMMMMMM! MM   `?MMRb.    `\"\"\"   !L\"MMMMM  XM IMMM\n"+
 "       MMMX   \"MMMM\"   MM       ~%:           !Mh.\"\"\"  dMI IMMP\n"+
 "       'MMM.                                               IMX\n"+
-"       ~M!M                                               IMP\n");
+"       ~M!M                                               IMP\n\n\n\n");
 	System.out.print("Bienvenido a PokemonJava.\n¿Qué desea hacer?\n1)batalla\t2)torneo\t3)hiatoria\t4)salir\n->");
 	opc=sc.next().toLowerCase().substring(0, 1);
 	batalla1vs1 b = new batalla1vs1(); 
 	switch (opc) {
 		case "1": b.main();break;
-		default: break;
+		case "2": break;
+		case "3": break;
+		case "4": break;
+		default:
+			System.out.println("Error al introducir opción. Pulse intro para continuar");
+			System.in.read();
+			break;
 	}
-	} while (opc!="4");
+	} while (opc.equals("4")==false);
 	}
 
 }
